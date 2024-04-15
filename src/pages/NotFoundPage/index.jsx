b22dbbfc871c6e0ca3afacc9e0860c76db90d3a0
Anna <anna.w099@gmail.com>
2024-04-15 17:12:51 +0200

    path

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,27 +1,7 @@
-import React, { useEffect } from "react";
 import style from "./style.module.css";
 import NotFound from "../../assets/images/notFound.svg";
-import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function NotFoundPage() {
-
-// 15.04
-  const navigate = useNavigate();
-	const location = useLocation();
-	useEffect(() => {
-		window.scrollTo(0, 0);
-		if (
-			location.pathname === '/GardenProject/' ||
-			location.pathname === '/GardenProject'
-		) {
-			navigate('/');
-		}
-	}, [location.pathname, navigate]);
-
-
-// 15.04
-
-
   return (
     <div className="container">
       <section className={style.notfound}>
